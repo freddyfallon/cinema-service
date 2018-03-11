fe2b47c7efaf3357c610e19f187262fbdf97d75f
Freddy Fallon <freddyfallon@gmail.com>
2018-03-11 09:55:27 +0000

    ♻️ Refactor cinemaController to use classes

--- Controllers/cinemaController.js
+++ Controllers/cinemaController.js
@@ -1,76 +1,77 @@
 const mongoose = require('mongoose');
 const Cinema = mongoose.model('Cinema');
 
-exports.createCinema = async (req, res) => {
-  try {
-    const cinema = new Cinema(req.body);
-    await cinema.save();
-    res.status(201);
-    res.send(`${cinema.name} was saved`);
-  } catch (err) {
-    const errorCode = err.name === 'ValidationError' ? 400 : 500;
-    res.status(errorCode);
-    res.send(`There was an error: ${err.toString()}`);
+module.exports = class cinemaController {
+  static async getCinemas(req, res) {
+    try {
+      const cinemas = await Cinema.find({}, (err, results) => {
+        return results;
+      });
+      res.status(200);
+      res.send(cinemas);
+    } catch (err) {
+      const errorCode = err.name === 'CastError' ? 400 : 500;
+      res.status(errorCode);
+      res.send(`There was an error: ${err.toString()}`);
+    }
+  }
+
+  static async getCinema(req, res) {
+    try {
+      const cinema = await Cinema.findById(req.params.id, (err, result) => {
+        return result;
+      });
+      res.status(200);
+      res.send(cinema);
+    } catch (err) {
+      const errorCode = err.name === 'CastError' ? 400 : 500;
+      res.status(errorCode);
+      res.send(`There was an error: ${err.toString()}`);
+    }
+  }
+
+  static async updateCinema(req, res) {
+    try {
+      const cinema = await Cinema.findById(req.params.id, (err, result) => {
+        return result;
+      });
+      await Cinema.update({ _id: cinema.id }, { $set: req.body }, () => {
+      });
+      res.status(200);
+      res.send(`${cinema.name} updated`);
+    } catch (err) {
+      const errorCode = err.name === 'CastError' ? 400 : 500;
+      res.status(errorCode);
+      res.send(`There was an error: ${err.toString()}`);
+    }
+  }
+
+  static async createCinema(req, res) {
+    try {
+      const cinema = new Cinema(req.body);
+      await cinema.save();
+      res.status(201);
+      res.send(`${cinema.name} was saved`);
+    } catch (err) {
+      const errorCode = err.name === 'ValidationError' ? 400 : 500;
+      res.status(errorCode);
+      res.send(`There was an error: ${err.toString()}`);
+    }
+  }
+
+  static async deleteCinema(req, res) {
+    try {
+      const cinema = await Cinema.findById(req.params.id, (err, result) => {
+        return result;
+      });
+      await Cinema.remove({ _id: cinema.id }, () => {
+      });
+      res.status(200);
+      res.send(`${cinema.name} was deleted`);
+    } catch (err) {
+      const errorCode = err.name === 'CastError' ? 400 : 500;
+      res.status(errorCode);
+      res.send(`There was an error: ${err.toString()}`);
+    }
   }
 };
-
-exports.getCinemas = async (req, res) => {
-  try {
-    const cinemas = await Cinema.find({}, (err, results) => {
-      return results;
-    });
-    res.status(200);
-    res.send(cinemas);
-  } catch (err) {
-    const errorCode = err.name === 'CastError' ? 400 : 500;
-    res.status(errorCode);
-    res.send(`There was an error: ${err.toString()}`);
-  }
-};
-
-exports.getCinema = async (req, res) => {
-  try {
-    const cinema = await Cinema.findById(req.params.id, (err, result) => {
-      return result;
-    });
-    res.status(200);
-    res.send(cinema);
-  } catch (err) {
-    const errorCode = err.name === 'CastError' ? 400 : 500;
-    res.status(errorCode);
-    res.send(`There was an error: ${err.toString()}`);
-  }
-};
-
-exports.deleteCinema = async (req, res) => {
-  try {
-    const cinema = await Cinema.findById(req.params.id, (err, result) => {
-      return result;
-    });
-    await Cinema.remove({ _id: cinema.id }, () => {
-    });
-    res.status(200);
-    res.send(`${cinema.name} was deleted`);
-  } catch (err) {
-    const errorCode = err.name === 'CastError' ? 400 : 500;
-    res.status(errorCode);
-    res.send(`There was an error: ${err.toString()}`);
-  }
-};
-
-exports.updateCinema = async (req, res) => {
-  try {
-    const cinema = await Cinema.findById(req.params.id, (err, result) => {
-      return result;
-    });
-    await Cinema.update({ _id: cinema.id }, { $set: req.body }, () => {
-    });
-    res.status(200);
-    res.send(`${cinema.name} updated`);
-  } catch (err) {
-    const errorCode = err.name === 'CastError' ? 400 : 500;
-    res.status(errorCode);
-    res.send(`There was an error: ${err.toString()}`);
-  }
-};
-
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const cinemaController = require('../Controllers/cinemaController');
+const {
+  createCinema, getCinemas, getCinema, deleteCinema, updateCinema
+} = require('../Controllers/cinemaController');
 
 router.post('/cinemas', (req, res) => {
-  cinemaController.createCinema(req, res);
+  createCinema(req, res);
 });
 
 router.get('/cinemas', (req, res) => {
-  cinemaController.getCinemas(req, res);
+  getCinemas(req, res);
 });
 
 router.get('/cinemas/:id', (req, res) => {
-  cinemaController.getCinema(req, res);
+  getCinema(req, res);
 });
 
 router.delete('/cinemas/:id', (req, res) => {
-  cinemaController.deleteCinema(req, res);
+  deleteCinema(req, res);
 });
 
 router.put('/cinemas/:id', (req, res) => {
-  cinemaController.updateCinema(req, res);
+  updateCinema(req, res);
 });
 
 module.exports = router;
