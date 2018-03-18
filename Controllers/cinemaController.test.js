2d15397449e023e0e85b1cc77937e3a62e429433
Freddy Fallon <freddyfallon@gmail.com>
2018-03-18 12:30:38 +0000

    ✅ Add tests for updateCinema

--- Controllers/cinemaController.test.js
+++ Controllers/cinemaController.test.js
@@ -57,4 +57,41 @@ describe('cinemaController', () => {
     });
   });
 
+  describe('updateCinema', () => {
+    const req = { params: { id: 'test ID' }, body: 'test body' };
+    const res = { status: jest.fn(), send: jest.fn() };
+
+    beforeEach(() => {
+      res.status.mockClear();
+      res.send.mockClear();
+    });
+
+
+    test('calls res.send, res.status, and returns a 200', async () => {
+      const response = { _id: 1, name: 'Chill cinema', description: 'A nice place' };
+      Cinema.findById = jest.fn().mockReturnValue(Promise.resolve(response));
+      Cinema.update = jest.fn();
+
+      await cinemaController.updateCinema(req, res);
+      expect(Cinema.findById).toBeCalledWith('test ID');
+      expect(res.status).toBeCalledWith(200);
+      expect(res.send).toBeCalledWith(`${response.name} updated`);
+    });
+
+    test('returns a 500 when cinema lookup fails', async () => {
+      Cinema.findById = jest.fn().mockReturnValue(Promise.reject(Error('words')));
+      await cinemaController.updateCinema(req, res);
+      expect(res.status).toBeCalledWith(500);
+      expect(res.send).toBeCalledWith('There was an error: Error: words');
+    });
+
+    test('returns a 500 when cinema update fails', async () => {
+      const response = { _id: 1, name: 'Chill cinema', description: 'A nice place' };
+      Cinema.findById = jest.fn().mockReturnValue(Promise.resolve(response));
+      Cinema.update = jest.fn().mockReturnValue(Promise.reject(Error('words')));
+      await cinemaController.updateCinema(req, res);
+      expect(res.status).toBeCalledWith(500);
+      expect(res.send).toBeCalledWith('There was an error: Error: words');
+    });
+  });
 });
