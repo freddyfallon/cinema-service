794ab7c0dc61a151c3373718ef717b4b6f6aed28
Freddy Fallon <freddyfallon@gmail.com>
2018-03-18 17:21:05 +0000

    ✅ Handle both 400 and 500 errors in tests

--- Controllers/cinemaController.test.js
+++ Controllers/cinemaController.test.js
@@ -22,11 +22,21 @@ describe('cinemaController', () => {
       expect(res.send).toBeCalledWith(response);
     });
 
-    test('returns a 500 when there is a failure', async () => {
-      Cinema.find = jest.fn().mockReturnValue(Promise.reject(Error('words')));
+    test('returns a 500 when there is a non CastError error', async () => {
+      const error = new Error('words');
+      Cinema.find = jest.fn().mockReturnValue(Promise.reject(error));
       await cinemaController.getCinemas(req, res);
       expect(res.status).toBeCalledWith(500);
-      expect(res.send).toBeCalledWith('There was an error: Error: words');
+      expect(res.send).toBeCalledWith(`There was an error: ${error}`);
+    });
+
+    test('returns a 400 when there is a CastError error', async () => {
+      const error = new Error('words');
+      error.name = 'CastError';
+      Cinema.find = jest.fn().mockReturnValue(Promise.reject(error));
+      await cinemaController.getCinemas(req, res);
+      expect(res.status).toBeCalledWith(400);
+      expect(res.send).toBeCalledWith(`There was an error: ${error}`);
     });
   });
 
@@ -49,11 +59,21 @@ describe('cinemaController', () => {
       expect(res.send).toBeCalledWith(response);
     });
 
-    test('returns a 500 when there is a failure', async () => {
-      Cinema.findById = jest.fn().mockReturnValue(Promise.reject(Error('words')));
+    test('returns a 500 when there is a non CastError error', async () => {
+      const error = new Error('words');
+      Cinema.findById = jest.fn().mockReturnValue(Promise.reject(error));
       await cinemaController.getCinema(req, res);
       expect(res.status).toBeCalledWith(500);
       expect(res.send).toBeCalledWith('There was an error: Error: words');
+    });
+
+    test('returns a 400 when there is a CastError error', async () => {
+      const error = new Error('words');
+      error.name = 'CastError';
+      Cinema.findById = jest.fn().mockReturnValue(Promise.reject(error));
+      await cinemaController.getCinema(req, res);
+      expect(res.status).toBeCalledWith(400);
+      expect(res.send).toBeCalledWith(`There was an error: ${error}`);
     });
   });
 
@@ -78,20 +98,45 @@ describe('cinemaController', () => {
       expect(res.send).toBeCalledWith(`${response.name} updated`);
     });
 
-    test('returns a 500 when cinema lookup fails', async () => {
+    test('returns a 500 when cinema lookup fails with non CastError', async () => {
+      const error = new Error('words');
       Cinema.findById = jest.fn().mockReturnValue(Promise.reject(Error('words')));
       await cinemaController.updateCinema(req, res);
       expect(res.status).toBeCalledWith(500);
-      expect(res.send).toBeCalledWith('There was an error: Error: words');
+      expect(res.send).toBeCalledWith(`There was an error: ${error}`);
     });
 
-    test('returns a 500 when cinema update fails', async () => {
+    test('returns a 400 when cinema lookup fails with CastError', async () => {
+      const error = new Error('words');
+      error.name = 'CastError';
+      Cinema.findById = jest.fn().mockReturnValue(Promise.reject(error));
+      await cinemaController.updateCinema(req, res);
+      expect(res.status).toBeCalledWith(400);
+      expect(res.send).toBeCalledWith(`There was an error: ${error}`);
+    });
+
+    test('returns a 500 when cinema update fails with non CastError', async () => {
+      const error = new Error('words');
       const response = { _id: 1, name: 'Chill cinema', description: 'A nice place' };
       Cinema.findById = jest.fn().mockReturnValue(Promise.resolve(response));
-      Cinema.update = jest.fn().mockReturnValue(Promise.reject(Error('words')));
+      Cinema.update = jest.fn().mockReturnValue(Promise.reject(error));
       await cinemaController.updateCinema(req, res);
       expect(res.status).toBeCalledWith(500);
-      expect(res.send).toBeCalledWith('There was an error: Error: words');
+      expect(res.send).toBeCalledWith(`There was an error: ${error}`);
+    });
+
+    test('returns a 400 when cinema update fails with CastError', async () => {
+      const error = new Error('words');
+      error.name = 'CastError';
+      const response = { _id: 1, name: 'Chill cinema', description: 'A nice place' };
+      Cinema.findById = jest.fn().mockReturnValue(Promise.resolve(response));
+      Cinema.update = jest.fn().mockReturnValue(Promise.reject(error));
+      await cinemaController.updateCinema(req, res);
+      expect(res.status).toBeCalledWith(400);
+      expect(res.send).toBeCalledWith(`There was an error: ${error}`);
+    });
+  });
+
     });
   });
 });
