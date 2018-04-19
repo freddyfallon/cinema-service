4346703366fbdb181545d267c0799b58405d235a
Freddy Fallon <freddyfallon@gmail.com>
2018-04-19 18:12:42 +0100

    ✨ Add typechecking across the application

--- routes/index.ts
+++ routes/index.ts
@@ -7,23 +7,23 @@ const {
 const database = require('../databases/mongodb/database');
 
 
-router.post('/cinemas', (req, res) => {
+router.post('/cinemas', (req: express.Request, res: express.Response) => {
   createCinema(database)(req, res);
 });
 
-router.get('/cinemas', (req, res) => {
+router.get('/cinemas', (req: express.Request, res: express.Response) => {
   getCinemas(database)(req, res);
 });
 
-router.get('/cinemas/:id', (req, res) => {
+router.get('/cinemas/:id', (req: express.Request, res: express.Response) => {
   getCinema(database)(req, res);
 });
 
-router.delete('/cinemas/:id', (req, res) => {
+router.delete('/cinemas/:id', (req: express.Request, res: express.Response) => {
   deleteCinema(database)(req, res);
 });
 
-router.put('/cinemas/:id', (req, res) => {
+router.put('/cinemas/:id', (req: express.Request, res: express.Response) => {
   updateCinema(database)(req, res);
 });
 
